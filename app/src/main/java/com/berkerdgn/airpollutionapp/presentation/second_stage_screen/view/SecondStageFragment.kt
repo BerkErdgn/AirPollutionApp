7962f7e5e89d8f2b56efffec5dfd7aa54b261342
Berk Erdoğan <erdgnberk@gmail.com>
2023-06-25 21:28:53 +0300

    Navigation and fragment
    Everything necessary for navigation and fragment has been done.

---
+++ app/src/main/java/com/berkerdgn/airpollutionapp/presentation/second_stage_screen/view/SecondStageFragment.kt
@@ -0,0 +1,55 @@
+package com.berkerdgn.airpollutionapp.presentation.second_stage_screen.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.berkerdgn.airpollutionapp.R
+import com.berkerdgn.airpollutionapp.databinding.FragmentSecondStageBinding
+import com.berkerdgn.airpollutionapp.presentation.list_screen.view.ListFragment
+import com.berkerdgn.airpollutionapp.presentation.save_screen.view.SavedFragment
+
+
+class SecondStageFragment : Fragment() {
+
+    private var _binding: FragmentSecondStageBinding?= null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentSecondStageBinding.inflate(inflater,container,false)
+        replaceFragment(ListFragment())
+        return  binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.navBar.setOnItemSelectedListener {
+            when (it){
+                R.id.home -> replaceFragment(ListFragment())
+                R.id.save -> replaceFragment(SavedFragment())
+            }
+            true
+        }
+
+    }
+
+
+    private fun replaceFragment (fragment: Fragment){
+        val fragmentManager = requireActivity().supportFragmentManager
+        val fragmentTransection = fragmentManager.beginTransaction()
+        fragmentTransection.replace(R.id.secondFragmentContainerView, fragment)
+        fragmentTransection.commit()
+    }
+
+}
---
+++ app/src/main/java/com/berkerdgn/airpollutionapp/presentation/splash_screen/view/SplashFragment.kt
@@ -0,0 +1,49 @@
+package com.berkerdgn.airpollutionapp.presentation.splash_screen.view
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.berkerdgn.airpollutionapp.R
+
+class SplashFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setTimer(view)
+    }
+
+
+    private fun setTimer (view: View){
+        val timer = object : CountDownTimer(3500,1000){
+            override fun onTick(p0: Long) {
+            }
+
+            override fun onFinish() {
+                val actionOneToSecond = SplashFragmentDirections.actionSplashFragmentToSecondStageFragment()
+                Navigation.findNavController(view).navigate(actionOneToSecond)
+            }
+
+        }
+
+        timer.start()
+    }
+}
